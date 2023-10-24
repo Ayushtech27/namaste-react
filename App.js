5e1ff2aefd3001914de5c9bd16b386ea6295275d
Ayush Singhal <singhal.ayush27@gmail.com>
2023-10-24 17:40:43 +0530

    Corrected minor bug

--- App.js
+++ App.js
@@ -1689,7 +1689,7 @@ const RestaurantCard = (props) => {
         alt="res-logo"
         src={
           "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-          resData.info.cloudinaryImageId
+          cloudinaryImageId
         }
       />
       <h3>{name}</h3>
