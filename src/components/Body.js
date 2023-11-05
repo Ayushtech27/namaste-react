b6749103866b90e162d94d6e8e0eb87176362d81
Ayush Singhal <singhal.ayush27@gmail.com>
2023-11-05 20:14:58 +0530

    Lecture 6: Did conditional rendering using ternary

--- src/components/Body.js
+++ src/components/Body.js
@@ -24,11 +24,10 @@ const Body = () => {
     );
   };
 
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
-
-  return (
+  //Conditional Rendering
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
