d188de9faf5e3208f34543f9d71e77cbe93306d8
Ayush Singhal <singhal.ayush27@gmail.com>
2023-11-10 22:44:37 +0530

    Lecture 7: Added an error page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
@@ -18,10 +20,15 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error />,
   },
   {
     path: "/about",
     element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
   },
 ]);
 
