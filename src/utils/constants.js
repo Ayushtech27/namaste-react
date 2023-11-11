41c454f6083cc06bad0db5cd4b7395dbaaf1f112
Ayush Singhal <singhal.ayush27@gmail.com>
2023-11-11 18:37:40 +0530

    Added dynamic routing for restaurant menu

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,6 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRarbFGOA8xl_IGcaB291JF16ZfgqEnfC5OxZn9xRFS&s";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.3972234&lng=85.357311&restaurantId=";
