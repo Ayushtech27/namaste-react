8cbf5dbe71a5719dbc5069b3148166e56bda9633
Ayush Singhal <singhal.ayush27@gmail.com>
2023-11-28 00:28:55 +0530

    Added useOnlineStatus hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -34,6 +35,15 @@ const Body = () => {
       console.error("Error fetching data:", error);
     }
   };
+
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you are offline!! Please check your internet connection.
+      </h1>
+    );
 
   //Conditional Rendering
   return !listOfRestaurants || listOfRestaurants.length === 0 ? (
