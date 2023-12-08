9503bea6c6a48fe15a569582715d03b44c703008
Ayush Singhal <singhal.ayush27@gmail.com>
2023-12-08 21:48:45 +0530

    Tailwind for shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,24 +1,35 @@
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
+    <div className="shimmer-container flex flex-wrap m-5">
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
+      <div className="m-4 h-[300px] p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200"></div>
     </div>
   );
 };
