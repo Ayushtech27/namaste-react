36c59bb64bf0db5ba967ab1f9db00db4c9d56dcc
Ayush Singhal <singhal.ayush27@gmail.com>
2024-02-17 03:27:23 +0530

    Configuring slice in the store

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
 
-const appStore = configureStore({});
+const appStore = configureStore({
+  // This reducer is basically our apps big reducer and it consists of small reducers from different slices.
+  reducer: {
+    //cart slice has cartReducer
+    cart: cartReducer,
+  },
+});
 
 export default appStore;
