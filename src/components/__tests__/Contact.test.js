9a706fa38c727b1e004f11959b5bba7a164c1e4d
Ayush Singhal <singhal.ayush27@gmail.com>
2024-03-05 02:34:58 +0530

    Replacing test keyword with it

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -3,7 +3,7 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 describe("Contact Us Page Test Cases", () => {
-  test("Should load contact us component", () => {
+  it("Should load contact us component", () => {
     render(<Contact />);
 
     const heading = screen.getByRole("heading");
@@ -12,7 +12,7 @@ describe("Contact Us Page Test Cases", () => {
     expect(heading).toBeInTheDocument();
   });
 
-  test("Should load button inside contact component", () => {
+  it("Should load button inside contact component", () => {
     render(<Contact />);
 
     const button = screen.getByRole("button");
@@ -21,7 +21,7 @@ describe("Contact Us Page Test Cases", () => {
     expect(button).toBeInTheDocument();
   });
 
-  test("Should load input name inside contact component", () => {
+  it("Should load input name inside contact component", () => {
     render(<Contact />);
 
     const inputName = screen.getByPlaceholderText("name");
@@ -30,7 +30,7 @@ describe("Contact Us Page Test Cases", () => {
     expect(inputName).toBeInTheDocument();
   });
 
-  test("Should load two input boxes on the contact component", () => {
+  it("Should load two input boxes on the contact component", () => {
     render(<Contact />);
 
     // Querying
