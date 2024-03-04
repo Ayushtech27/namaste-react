c7aaf3638fe45dadacabdf34ec53a0b61f6ce5bd
Ayush Singhal <singhal.ayush27@gmail.com>
2024-03-05 02:27:30 +0530

    Writing testcases for the Contact component

---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+test("Should load contact us component", () => {
+  render(<Contact />);
+
+  const heading = screen.getByRole("heading");
+
+  //Assersion
+  expect(heading).toBeInTheDocument();
+});
+
+test("Should load button inside contact component", () => {
+  render(<Contact />);
+
+  const button = screen.getByRole("button");
+
+  // Assersion
+  expect(button).toBeInTheDocument();
+});
+
+test("Should load input name inside contact component", () => {
+  render(<Contact />);
+
+  const inputName = screen.getByPlaceholderText("name");
+
+  // Assersion
+  expect(inputName).toBeInTheDocument();
+});
+
+test("Should load two input boxes on the contact component", () => {
+  render(<Contact />);
+
+  // Querying
+  const inputBoxes = screen.getAllByRole("textbox");
+
+  // Assersion
+  expect(inputBoxes.length).toBe(2);
+});
