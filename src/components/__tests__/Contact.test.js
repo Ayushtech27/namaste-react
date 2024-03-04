81f532e48f394a867b41ee3be436da1ebb395325
Ayush Singhal <singhal.ayush27@gmail.com>
2024-03-05 02:31:18 +0530

    Grouping Contact Us page test cases

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,39 +2,41 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />);
+describe("Contact Us Page Test Cases", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading");
 
-  //Assersion
-  expect(heading).toBeInTheDocument();
-});
+    //Assersion
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should load button inside contact component", () => {
-  render(<Contact />);
+  test("Should load button inside contact component", () => {
+    render(<Contact />);
 
-  const button = screen.getByRole("button");
+    const button = screen.getByRole("button");
 
-  // Assersion
-  expect(button).toBeInTheDocument();
-});
+    // Assersion
+    expect(button).toBeInTheDocument();
+  });
 
-test("Should load input name inside contact component", () => {
-  render(<Contact />);
+  test("Should load input name inside contact component", () => {
+    render(<Contact />);
 
-  const inputName = screen.getByPlaceholderText("name");
+    const inputName = screen.getByPlaceholderText("name");
 
-  // Assersion
-  expect(inputName).toBeInTheDocument();
-});
+    // Assersion
+    expect(inputName).toBeInTheDocument();
+  });
 
-test("Should load two input boxes on the contact component", () => {
-  render(<Contact />);
+  test("Should load two input boxes on the contact component", () => {
+    render(<Contact />);
 
-  // Querying
-  const inputBoxes = screen.getAllByRole("textbox");
+    // Querying
+    const inputBoxes = screen.getAllByRole("textbox");
 
-  // Assersion
-  expect(inputBoxes.length).toBe(2);
+    // Assersion
+    expect(inputBoxes.length).toBe(2);
+  });
 });
